ea5864a6d36f3e0e4266bd01c0cda87a2015b45c
BABATUNDE, Ibukun <96484818+babatundeibukun@users.noreply.github.com>
2022-03-17 04:41:38 -0700

    Add files via upload

---
+++ birthday wisher project/birthday wisher project/main.py
@@ -0,0 +1,36 @@
+import datetime as dt
+import pandas as pd
+import random
+import smtplib
+
+now = dt.datetime.now()
+today_month = now.month
+today_day = now.day
+today = (today_month, today_day)
+data = pd.read_csv("birthdays.csv")
+
+birthdays_dict = {(row.month, row.day): row
+                  for (index, row) in data.iterrows()}
+
+with open("letter_templates/letter_1.txt") as first_letter:
+    first = first_letter.read()
+with open("letter_templates/letter_2.txt") as first_letter:
+    second = first_letter.read()
+with open("letter_templates/letter_3.txt") as first_letter:
+    third = first_letter.read()
+
+list_letters = (first, second, third)
+
+if (today_month, today_day) in birthdays_dict:
+    birthday_person = (birthdays_dict[today])
+    random_index = random.randint(0, 2)
+    letter = list_letters[random_index]
+    final_letter = letter.replace("[NAME]", birthday_person["name"])
+    my_email = "inputyouremail@gmail.com"
+    password = "inputyourpassword"
+    # make sure you have changed the setting of your email to remove secure connection
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs=birthday_person['email'],
+                            msg=f"Subject:Happy Birthday\n\n{final_letter}")
